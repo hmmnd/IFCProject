2136ac6921c50b973abb5ebb9afd75394aaf0d77
hmmnd <raymond-hammond@uiowa.edu>
2016-03-23 10:39:50 -0500

    Update Book.java

--- src/excelwriter/Book.java
+++ src/excelwriter/Book.java
@@ -1,5 +1,18 @@
 package excelwriter;
 
 public class Book {
-
+    private String title;
+    private String author;
+    private float price;
+ 
+    public Book() {
+    }
+ 
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+ 
+    // getters and setters
 }
