3b57e1ada67355026a87935ec3bffcb5ada66f49
hmmnd <raymond-hammond@uiowa.edu>
2016-03-23 10:42:22 -0500

    Update writeBook.java

--- src/excelwriter/writeBook.java
+++ src/excelwriter/writeBook.java
@@ -1,5 +1,12 @@
 package excelwriter;
 
-public class writeBook {
-
+private void writeBook(Book aBook, Row row) {
+    Cell cell = row.createCell(1);
+    cell.setCellValue(aBook.getTitle());
+ 
+    cell = row.createCell(2);
+    cell.setCellValue(aBook.getAuthor());
+ 
+    cell = row.createCell(3);
+    cell.setCellValue(aBook.getPrice());
 }
