b85f3f934468a39b07a56221c51b8f0b48d7b347
hmmnd <raymond-hammond@uiowa.edu>
2016-03-23 10:42:52 -0500

    Update writeExcel.java

--- src/excelwriter/writeExcel.java
+++ src/excelwriter/writeExcel.java
@@ -1,5 +1,17 @@
 package excelwriter;
 
-public class writeExcel {
-
+public void writeExcel(List<Book> listBook, String excelFilePath) throws IOException {
+    Workbook workbook = new HSSFWorkbook();
+    Sheet sheet = workbook.createSheet();
+ 
+    int rowCount = 0;
+ 
+    for (Book aBook : listBook) {
+        Row row = sheet.createRow(++rowCount);
+        writeBook(aBook, row);
+    }
+ 
+    try (FileOutputStream outputStream = new FileOutputStream(excelFilePath)) {
+        workbook.write(outputStream);
+    }
 }
