40f4695aca16b622656c394fb5dc5549e5c94b72
hmmnd <raymond-hammond@uiowa.edu>
2016-03-23 10:40:45 -0500

    Update getListBook.java

--- src/excelwriter/getListBook.java
+++ src/excelwriter/getListBook.java
@@ -1,5 +1,12 @@
 package excelwriter;
 
-public class getListBook {
-
+private List<Book> getListBook() {
+    Book book1 = new Book("Head First Java", "Kathy Serria", 79);
+    Book book2 = new Book("Effective Java", "Joshua Bloch", 36);
+    Book book3 = new Book("Clean Code", "Robert Martin", 42);
+    Book book4 = new Book("Thinking in Java", "Bruce Eckel", 35);
+ 
+    List<Book> listBook = Arrays.asList(book1, book2, book3, book4);
+ 
+    return listBook;
 }
