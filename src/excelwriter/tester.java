a24e5945e783735240f0acc62edfca57ce9fbdf6
hmmnd <raymond-hammond@uiowa.edu>
2016-03-23 10:41:49 -0500

    Update tester.java

--- src/excelwriter/tester.java
+++ src/excelwriter/tester.java
@@ -4,7 +4,12 @@ public class tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		NiceExcelWriterExample excelWriter = new NiceExcelWriterExample();
+ 
+		List<Book> listBook = excelWriter.getListBook();
+		String excelFilePath = "NiceJavaBooks.xls";
+ 
+		excelWriter.writeExcel(listBook, excelFilePath);
 	}
 
 }
